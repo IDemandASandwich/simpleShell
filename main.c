3f038cf98dee9aab7903a3eacb57a7e68fc9b916
Andrej Modránszky <xmodranszky@stuba.sk>
2024-08-21 14:51:02 +0200

    end

--- main.c
+++ main.c
@@ -7,6 +7,11 @@
 void lsh_loop(void);
 char *lsh_read_line(void);
 char **lsh_split_line(char *line);
+int lsh_launch(char **args);
+
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
 
 int main(int argc, char **argv){
 
@@ -77,4 +82,87 @@ char **lsh_split_line(char *line){
 
     tokens[position] == NULL;
     return tokens;
+}
+
+int lsh_launch(char **args){
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if(pid == 0){
+        if(execvp(args[0], args) == -1){
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if(pid < 0){
+        perror("lsh");
+    }
+    else{
+        do{
+            wpid = waitpid(pid, &status, WUNTRACED);
+        }while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+int (*builtin_func[])(char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit
+};
+
+int lsh_num_builtins(){
+    return sizeof(builtin_str) / sizeof(char*);
+}
+
+int lsh_cd(char **args){
+    if(args[1] == NULL){
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    }
+    else{
+        if(chdir(args[1]) != 0){
+            perror("lsh");
+        }
+    }
+
+    return 1;
+}
+
+int lsh_help(char **args){
+    int i;
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for(i = 0; i < lsh_num_builtins(); i++){
+        printf("  %s\n",builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int lsh_exit(char **args){
+    return 0;
+}
+
+int lsh_execute(char **args){
+    if(args[0] == NULL){
+        return 1;
+    }
+
+    for(int i = 0; i < lsh_num_builtins(); i++){
+        if(strcmp(args[0], buuiltin_str[i]) == 0){
+            return(*builtin_func[i])(args);
+        }
+    }
+
+    return lsh_launch(args);
 }
